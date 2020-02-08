d16a89b54c249ecf7dbd9f6a2698a97a027b110f
amiralii <mr.roshanae@gmail.com>
2020-02-08 19:08:49 +0330

    fixed

--- controllers/on_media.events.go
+++ controllers/on_media.events.go
@@ -7,77 +7,73 @@ import (
 
 func onMediaEvents(app *config.App, bot *tb.Bot) {
 
-	db := app.DB()
-	defer db.Close()
-	lastState := GetUserLastState(db, app, bot, message, message.Sender.ID)
+	bot.Handle(tb.OnPhoto, func(message *tb.Message) {
+		if !message.Private() {
+			return
+		}
+		db := app.DB()
+		defer db.Close()
+		lastState := GetUserLastState(db, app, bot, message, message.Sender.ID)
 
-	if lastState.State == config.LangConfig.GetString("STATE.NEW_MESSAGE_TO_GROUP") ||
-		lastState.State == config.LangConfig.GetString("STATE.REPLY_TO_MESSAGE") ||
-		lastState.State == config.LangConfig.GetString("STATE.REPLY_BY_DM") ||
-		lastState.State == config.LangConfig.GetString("STATE.ANSWER_TO_DM") {
-
-		bot.Handle(tb.OnPhoto, func(message *tb.Message) {
-			if !message.Private() {
-				return
-			}
-
-			switch {
-			case lastState.State == config.LangConfig.GetString("STATE.NEW_MESSAGE_TO_GROUP"):
-				goto SaveAndSendMessage
-			case lastState.State == config.LangConfig.GetString("STATE.REPLY_TO_MESSAGE"):
-				goto SendAndSaveReplyMessage
-			case lastState.State == config.LangConfig.GetString("STATE.REPLY_BY_DM"):
-				goto SendAndSaveDirectMessage
-			case lastState.State == config.LangConfig.GetString("STATE.ANSWER_TO_DM"):
-				goto SendAnswerAndSaveDirectMessage
-			default:
-				goto END
-			}
-
-		SaveAndSendMessage:
-			if inlineOnTextEventsHandler(app, bot, message, db, lastState, &Event{
-				UserState:  config.LangConfig.GetString("STATE.NEW_MESSAGE_TO_GROUP"),
-				Command:    config.LangConfig.GetString("STATE.COMPOSE_MESSAGE") + "_",
-				Controller: "SaveAndSendMessage",
-			}) {
-				Init(app, bot, true)
-			}
+		switch {
+		case lastState.State == config.LangConfig.GetString("STATE.NEW_MESSAGE_TO_GROUP"):
+			goto SaveAndSendMessage
+		case lastState.State == config.LangConfig.GetString("STATE.REPLY_TO_MESSAGE"):
+			goto SendAndSaveReplyMessage
+		case lastState.State == config.LangConfig.GetString("STATE.REPLY_BY_DM"):
+			goto SendAndSaveDirectMessage
+		case lastState.State == config.LangConfig.GetString("STATE.ANSWER_TO_DM"):
+			goto SendAnswerAndSaveDirectMessage
+		case lastState.State == config.LangConfig.GetString("STATE.ADD_PSEUDONYM"):
+			bot.Send(m.Sender, config.LangConfig.GetString("MESSAGES.USER_NAME_IS_WRONG"))
 			goto END
-
-		SendAndSaveReplyMessage:
-			if inlineOnTextEventsHandler(app, bot, message, db, lastState, &Event{
-				UserState:  config.LangConfig.GetString("STATE.REPLY_TO_MESSAGE"),
-				Command:    config.LangConfig.GetString("STATE.REPLY_TO_MESSAGE") + "_",
-				Command1:   config.LangConfig.GetString("COMMANDS.START_REPLY"),
-				Controller: "SendAndSaveReplyMessage",
-			}) {
-				Init(app, bot, true)
-			}
+		default:
+			bot.Send(m.Sender, "Your message is not being processed or sent to any individual, channel or group.")
 			goto END
+		}
 
-		SendAndSaveDirectMessage:
-			if inlineOnTextEventsHandler(app, bot, message, db, lastState, &Event{
-				UserState:  config.LangConfig.GetString("STATE.REPLY_BY_DM"),
-				Command:    config.LangConfig.GetString("STATE.REPLY_BY_DM") + "_",
-				Command1:   config.LangConfig.GetString("COMMANDS.START_REPLY_DM"),
-				Controller: "SendAndSaveDirectMessage",
-			}) {
-				Init(app, bot, true)
-			}
-			goto END
+	SaveAndSendMessage:
+		if inlineOnTextEventsHandler(app, bot, message, db, lastState, &Event{
+			UserState:  config.LangConfig.GetString("STATE.NEW_MESSAGE_TO_GROUP"),
+			Command:    config.LangConfig.GetString("STATE.COMPOSE_MESSAGE") + "_",
+			Controller: "SaveAndSendMessage",
+		}) {
+			Init(app, bot, true)
+		}
+		goto END
 
-		SendAnswerAndSaveDirectMessage:
-			if inlineOnTextEventsHandler(app, bot, message, db, lastState, &Event{
-				UserState:  config.LangConfig.GetString("STATE.ANSWER_TO_DM"),
-				Command:    config.LangConfig.GetString("STATE.ANSWER_TO_DM") + "_",
-				Controller: "SendAnswerAndSaveDirectMessage",
-			}) {
-				Init(app, bot, true)
-			}
-			goto END
+	SendAndSaveReplyMessage:
+		if inlineOnTextEventsHandler(app, bot, message, db, lastState, &Event{
+			UserState:  config.LangConfig.GetString("STATE.REPLY_TO_MESSAGE"),
+			Command:    config.LangConfig.GetString("STATE.REPLY_TO_MESSAGE") + "_",
+			Command1:   config.LangConfig.GetString("COMMANDS.START_REPLY"),
+			Controller: "SendAndSaveReplyMessage",
+		}) {
+			Init(app, bot, true)
+		}
+		goto END
 
-		END:
-		})
+	SendAndSaveDirectMessage:
+		if inlineOnTextEventsHandler(app, bot, message, db, lastState, &Event{
+			UserState:  config.LangConfig.GetString("STATE.REPLY_BY_DM"),
+			Command:    config.LangConfig.GetString("STATE.REPLY_BY_DM") + "_",
+			Command1:   config.LangConfig.GetString("COMMANDS.START_REPLY_DM"),
+			Controller: "SendAndSaveDirectMessage",
+		}) {
+			Init(app, bot, true)
+		}
+		goto END
 
-	}
+	SendAnswerAndSaveDirectMessage:
+		if inlineOnTextEventsHandler(app, bot, message, db, lastState, &Event{
+			UserState:  config.LangConfig.GetString("STATE.ANSWER_TO_DM"),
+			Command:    config.LangConfig.GetString("STATE.ANSWER_TO_DM") + "_",
+			Controller: "SendAnswerAndSaveDirectMessage",
+		}) {
+			Init(app, bot, true)
+		}
+		goto END
+
+	END:
+	})
 }
