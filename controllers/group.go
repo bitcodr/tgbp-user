dfcef1881bd60d95cbb17acd7e24a7e4e7c55de1
amiralii <mr.roshanae@gmail.com>
2020-03-08 19:56:50 +0330

    fixed

--- controllers/group.go
+++ controllers/group.go
@@ -91,7 +91,9 @@ func (service *BotService) RegisterGroup(app *config.App, bot *tb.Bot, m *tb.Mes
 			}
 			sendOptionModel := new(tb.SendOptions)
 			sendOptionModel.ParseMode = tb.ModeHTML
-			_, err = bot.Send(m.Chat, config.LangConfig.GetString("MESSAGES.GROUP_UNIQUE_ID_MESSAGE")+"<code> "+uniqueID+" </code>", sendOptionModel)
+							_, err = bot.Send(m.Chat, config.LangConfig.GetString("MESSAGES.GROUP_UNIQUE_ID_MESSAGE")+
+
+				` <code> `+uniqueID+` </code>`, sendOptionModel)
 			if err != nil {
 				log.Println(err)
 				return true
